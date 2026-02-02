c4de40135b9abdc5b44bdb11dc0aaf91d82ed7a2
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-02-02 18:50:44 +0800

    Initial Commit

---
+++ Get/GetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetApp.swift
+//  Get
+//
+//  Created by Kelvin Ricafort on 2/2/26.
+//
+
+import SwiftUI
+
+@main
+struct GetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
